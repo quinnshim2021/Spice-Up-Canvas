1b17c163b226b1e958d0a559e948f94c8343faae
quinnshim2021 <quinnshim2021@u.northwestern.edu>
2020-12-14 01:47:32 -0500

    Updated content

--- content.js
+++ content.js
@@ -5,14 +5,19 @@ const replaceHeaders = () => {
     let headerHeros = document.getElementsByClassName("ic-DashboardCard__header_hero");
 
     let src = [chrome.extension.getURL("./images/td.gif"), 
-               chrome.extension.getURL("./images/bakugo.gif"), 
-               chrome.extension.getURL("./images/jotaro.gif"), 
-               chrome.extension.getURL("./images/mista.gif")];
+               chrome.extension.getURL("./images/wwy.gif"), 
+               chrome.extension.getURL("./images/tokyo.gif"), 
+               chrome.extension.getURL("./images/mista.gif"),
+               chrome.extension.getURL("./images/bakugo.gif"),
+               chrome.extension.getURL("./images/caesar.gif"),
+               chrome.extension.getURL("./images/vaporeon.gif"),
+               chrome.extension.getURL("./images/yourname.gif"),
+               chrome.extension.getURL("./images/jotaro.gif")];
 
     let count = 0;
     for (let box of headerHeros){
         if (src[count]){
-            box.setAttribute('style', `background: url(${src[count]}) !important`);
+            box.setAttribute('style', `background: url(${src[count]}) !important; background-size: cover !important; background-repeat: no-repeat !important;`);
             count ++;
         }
         else{
