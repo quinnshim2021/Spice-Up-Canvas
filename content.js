8e16262fe408ecb6ccdd79c5df27a3ae65ee3352
quinnshim2021 <quinnshim2021@u.northwestern.edu>
2020-12-14 23:09:28 -0500

    able to browse saved images

--- content.js
+++ content.js
@@ -27,55 +27,58 @@ const replaceHeaders = (src) => {
     }
 }
 
-const saveImages = (s) => {
+const saveImages = async (s) => {
     if (s === undefined || s.length === 0){ return; }
     else{
-        chrome.storage.sync.get(['src'], function(result){
-            console.log(`Images retrieved`);
-            console.log(result['src']);
-    
-            let src = [];
-            for (let i of result['src']){
-                src.push(i);
-            }
-            for (let i of s){
-                if (!src.includes(i)){ // for no duplicates
+        return new Promise(function(resolve, reject) {chrome.storage.sync.get(['src'], function(result){
+                console.log(`Images to save retrieved`);
+                //console.log(result['src']);
+                let src = [];
+                for (let i of result['src']){
                     src.push(i);
                 }
-            }
-            console.log(src);
+                for (let i of s){
+                    if (!src.includes(i)){ // for no duplicates
+                        src.push(i);
+                    }
+                }
+                //console.log(src);
+                resolve(src);
 
-            chrome.storage.sync.set({'src': src}, function(){
-                console.log(`Images saved:`);
-                console.log(src);
-                updateHeaders(src);
-            });
-        }); 
+                chrome.storage.sync.set({'src': src}, function(){
+                    console.log(`Images saved:`);
+                    //console.log(src);
+                    updateHeaders(src);
+                });
+            }) 
+        });
     }
 }
 
-const deleteImages = (s) => {
+const deleteImages = async (s) => {
     if (s === undefined || s.length === 0){ return; }
     else{
-        chrome.storage.sync.get(['src'], function(result){
-            console.log(`Images retrieved`);
-            console.log(result['src']);
-    
+        return new Promise(function(resolve, reject) {chrome.storage.sync.get(['src'], function(result){
+            console.log(`Images to delete retrieved`);
+            //console.log(result['src']);
             let src = [];
             for (let i of result['src']){
                 if (!s.includes(i)){
                     src.push(i);
                 }
             }
-            console.log(src);
-
+            //console.log(src);
+            resolve(src);
             chrome.storage.sync.set({'src': src}, function(){
                 console.log(`Images saved:`);
-                console.log(src);
+                //console.log(src);
                 updateHeaders(src);
             });
+         })
         }); 
     }
+    
+    return src;
 }
 
 const updateHeaders = (s) => {
@@ -100,20 +103,24 @@ const updateHeaders = (s) => {
 
 const gotMessage = (message, sender, sendResponse) => {
     console.log(`Message recieved:`);
-    console.log(message);
     if (message.command === "save"){
-       saveImages(message.data);
+        saveImages(message.data).then((res) => {
+            console.log(res);
+            sendResponse({txt: "updated saved", data: res});
+        }); // watch out here if uploaded photo is not returned with list of saved
     } else if (message.command === "delete") {
-        deleteImages(message.data);
+        deleteImages(message.data).then((res) => {
+            sendResponse({txt: "updated saved", data: res})});
     } else{
         console.log("idk what to do with this");
     }
+    return true;
 }
 
 window.addEventListener('load', () => {
     chrome.storage.sync.get(['src'], function(result){
-        console.log(`Images retrieved`);
-        console.log(result['src']);
+        console.log(`Images loaded`);
+        //console.log(result['src']);
 
         let src = [];
         for (let i of result['src']){
--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,11 @@
+let G_SRC = [];
+let PAGE = 0; // ASSUME 4 PAGES, automate based on result list size later
+let G_SAVED = [];
+let STACK = "trending";
+
 // toggle what is selected
-const toggleSelection = (option) => {
-    option.classList.toggle("selected");
+const toggleSelection = (option, c) => {
+    option.classList.toggle(c);
 }
 
 // sends array of gif objects to content
@@ -20,17 +25,69 @@ const run = (command) => {
             command: command,
             data: src
         };
-        
-        chrome.tabs.sendMessage(tabs[0].id, msg);
+        chrome.tabs.sendMessage(tabs[0].id, msg, (response) =>{
+            G_SAVED = response.data;
+            console.log(response);
+        });
     });
 }
 
-// gets 5 trending gives data to run
-// add them to popup
-const giphy = () => {
-    let api_key = env["API_KEY"];
+const displayCards = (page) => {
+    document.getElementById("page").innerText = page + 1;
+    let src = STACK === "saved" ? G_SAVED : G_SRC;
+    let grid = document.getElementById("grid-container");
+    while (grid.firstChild){
+        grid.removeChild(grid.firstChild);
+    }
+    let count = 0;
+    for (let gif of src) {
+        if ((count - (page*4) <= 3) && (count - (page*4) >= 0)){
+            if ((page+1)*4 > count){
+                let div = document.createElement("div");
+                div.classList = "grid-item option";
+                div.src = gif;
+                div.setAttribute('style', `background: url(${gif}) !important; background-size: cover !important; background-repeat: no-repeat !important;`);
+                grid.appendChild(div);
+                count ++;
+            }
+        }
+        else{
+            count++;
+            continue;
+        }
+    }
+
+    for(let option of document.getElementsByClassName('option')) {
+        option.addEventListener('click', (event) => {
+            toggleSelection(option, "selected");
+        });
+    };
+}
+
+const newResults = (id) => {
+    if (id === "-1"){
+        if (PAGE - 1 >= 0){
+            PAGE--;
+            document.getElementById("page").innerText = PAGE + 1;
+        }
+        displayCards(PAGE);
+        console.log(`Page: ${PAGE}`);
+    } else if (id === "1") {
+        if (PAGE + 1 < 4){
+            PAGE++; 
+            document.getElementById("page").innerText = PAGE + 1;
+        }
+        displayCards(PAGE);
+        console.log(`Page: ${PAGE}`);
+    }
     
-    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=4`)
+};
+
+// default with trending
+// add them to popup
+const giphy = () => {    
+    let api_key = env["API_KEY"];
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=16`)
         .then(response => {
             return response.json();
         })
@@ -43,26 +100,66 @@ const giphy = () => {
             }
 
             /* add to popup.html here */
-            let grid = document.getElementById("grid-container");
-            for (let gif of src) {
-                let div = document.createElement("div");
-                div.classList = "grid-item option";
-                div.src = gif;
-                div.setAttribute('style', `background: url(${gif}) !important; background-size: cover !important; background-repeat: no-repeat !important;`);
-                grid.appendChild(div);
-            }
-
-            for(let option of document.getElementsByClassName('option')) {
-                option.addEventListener('click', (event) => {
-                    toggleSelection(option);
-                });
-            };
+            G_SRC = src;
+            PAGE = 0;
+            STACK = "trending";
+            displayCards(PAGE);
         })
         .catch(() => {
             console.log("error");
         });
 }
 
+const displaySearch = () => {
+    let api_key = env["API_KEY"];
+    let userinput = document.getElementById("userinput").value;
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&limit=16&q=${userinput}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            let src = [];
+            for (let m of response.data){
+                console.log(`${m["id"]}: ${m["images"]["downsized_medium"]["url"]}`);
+                let id = m["images"]["downsized_small"]["mp4"].split("media/")[1].split("/giphy")[0];
+                src.push(`https://i.giphy.com/${id}.gif`);
+            }
+
+            /* add to popup.html here */
+            G_SRC = src;
+            PAGE = 0;
+            STACK = "searched";
+            displayCards(PAGE);
+        })
+        .catch(() => {
+            console.log("error");
+        });
+};
+
+const loadListeners = () => {
+    for(let option of document.getElementsByClassName("pagination")[0].childNodes) {
+        option.addEventListener('click', (event) => {
+            event.preventDefault();
+            newResults(option.id);
+        });
+    };
+
+    document.getElementById("trending").addEventListener("click", (event) => {
+        event.preventDefault();
+        giphy();
+    });
+
+    giphy();
+};
+
+const displaySaved = () => {
+    PAGE = 0;
+    STACK = "saved";
+    displayCards(PAGE);
+};
+
+document.getElementById("search").addEventListener("click", (e) => {e.preventDefault(); displaySearch()}, false);
 document.getElementById("submit").addEventListener("click", (e) => {e.preventDefault(); run("save")}, false);
+document.getElementById("saved").addEventListener("click", (e) => {e.preventDefault(); displaySaved()}, false);
 document.getElementById("delete").addEventListener("click", (e) => {e.preventDefault(); run("delete")}, false);
-window.addEventListener('load', giphy, false);
+window.addEventListener('load', loadListeners, false);
